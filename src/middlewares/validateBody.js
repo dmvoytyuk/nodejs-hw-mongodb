84e077ffaf5a82973bd584a9e08afb2fdade8678
dmvoytyuk <dmvoytyuk@gmail.com>
2024-06-18 19:11:50 +0200

    validateBody fix

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -3,6 +3,7 @@ import createHttpError from 'http-errors';
 export const validateBody = (schema) => async (req, _res, next) => {
   try {
     await schema.validateAsync(req.body, { abortEarlt: false });
+    next();
   } catch (err) {
     next(createHttpError(400, 'Bad request', { errors: err.details }));
   }
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,7 +4,7 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
+  isFavorite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
@@ -12,6 +12,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
+  isFavorite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
